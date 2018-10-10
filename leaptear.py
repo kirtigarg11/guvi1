ef5aa57cd0e54c186a1592944536b46b5f34c54e
kirtigarg11 <40167262+kirtigarg11@users.noreply.github.com>
2018-10-10 13:53:31 -0700

    Update leaptear.py

--- leaptear.py
+++ leaptear.py
@@ -1,5 +1,13 @@
 n=int(input())
 if(n%4==0):
-    print("yes")
+    if(n%100==0):
+        if(n%400==0):
+            print("yes")
+        else:
+            print("no")
+    else:
+        print("yes")
+    
 else:
+    
     print("no")
